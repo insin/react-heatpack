895c46e6f1fee5f5df2eb426b420b2863a87fa49
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-07-02 17:55:47 +0100

    Display version with -v flag

--- bin/heatpack.js
+++ bin/heatpack.js
@@ -3,18 +3,24 @@
 var path = require('path')
 var parseArgs  = require('minimist')
 
+var pkg = require('../package.json')
 var config = require('../webpack.config')
 var server = require('../server')
 
 var args = parseArgs(process.argv.slice(2), {
   alias: {
-    h: 'help'
+    h: 'help',
+    v: 'version'
   }
 })
 
+if (args.version) {
+  console.log('v' + pkg.version)
+  process.exit(0)
+}
+
 if (args.help || args._.length === 0) {
-  console.log('Usage: heatpack entry_module')
-  console.log('')
+  console.log('Usage: heatpack script.js')
   process.exit(0)
 }
 
