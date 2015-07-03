cf44cf8e3251aafaf119da047ce154b4f73cca46
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-07-03 11:45:59 +0100

    Remove optional last character from CoffeeScript/CJSX loader test config

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,8 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.jsx?$/, loader: 'react-hot!babel?stage=0', exclude: /node_modules/},
-      {test: /\.cjsx?$/, loader: 'react-hot!coffee!cjsx', exclude: /node_modules/},
-      {test: /\.coffee?$/, loader: 'coffee', exclude: /node_modules/},
+      {test: /\.cjsx$/, loader: 'react-hot!coffee!cjsx', exclude: /node_modules/},
+      {test: /\.coffee$/, loader: 'coffee', exclude: /node_modules/},
       {test: /\.json$/, loader: 'json'},
       {test: /\.css$/, loader: 'style!css?-restructuring!autoprefixer'},
       {test: /\.(gif|jpe?g|png|otf|eot|svg|ttf|woff|woff2).*$/, loader: 'url?limit=8192'}
