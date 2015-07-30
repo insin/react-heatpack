4ab85853012df5a316e8197ce161bd25129d0a05
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-07-30 16:09:44 +0100

    Needless frippery

--- test/Stories.js
+++ test/Stories.js
@@ -6,34 +6,60 @@ async function fetchStories(subreddit) {
   return json
 }
 
+var SUBREDDITS = ['javascript', 'programming', 'deepdream']
+
 var Stories = React.createClass({
   getInitialState() {
     return {
       loading: false,
-      stories: []
+      stories: [],
+      subreddit: 'javascript'
     }
   },
+
   componentDidMount() {
     this.updateStories()
   },
+
   async updateStories() {
-    this.setState({loading: true})
+    var loadingTimeout = setTimeout(() => {
+      this.setState({loading: true})
+      loadingTimeout = null
+    }, 333)
+
     try {
-      var data = await fetchStories('javascript')
+      var data = await fetchStories(this.state.subreddit)
       this.setState({stories: data.data.children.map(child => child.data)})
     }
     catch (e) {
       // pass
     }
-    this.setState({loading: false})
+
+    if (loadingTimeout) {
+      clearTimeout(loadingTimeout)
+    }
+    else {
+      this.setState({loading: false})
+    }
   },
+
+  handleChange(e) {
+    var subreddit = e.target.value
+    this.setState({subreddit}, this.updateStories)
+  },
+
   render() {
     return <div className="Stories">
-      <h2>Stories</h2>
+      <h2>
+        <select disabled={this.state.loading} value={this.state.subreddit} onChange={this.handleChange}>
+          {SUBREDDITS.map(subreddit => <option key={subreddit}>{subreddit}</option>)}
+        </select>{' '}
+        Stories
+      </h2>
       {this.state.loading && <h3>Loading...</h3>}
-      <ul>
+      <ol style={{opacity: this.state.loading ? '.5' : '1'}}>
         {this.state.stories.map(story => <li key={story.id}>{story.title}</li>)}
-      </ul>
+      </ol>
     </div>
   }
 })
