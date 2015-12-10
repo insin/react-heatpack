a10add57e532402fdf4eb1201fbb00400b35fdcb
Patrick Williams <pwmckenna@gmail.com>
2015-12-10 08:09:22 -0800

    no more babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,14 @@ module.exports = function config(options) {
           loader: require.resolve('babel-loader'),
           exclude: excludeJS,
           query: {
+            presets: [
+              require.resolve('babel-preset-es2015'),
+              require.resolve('babel-preset-react'),
+              require.resolve('babel-preset-stage-0')
+            ],
             plugins: [
+              require.resolve('babel-plugin-transform-runtime'),
+              require.resolve('babel-plugin-transform-react-display-name'),
               ['react-transform', {
                 transforms: [{
                   transform: require.resolve('react-transform-hmr'),
